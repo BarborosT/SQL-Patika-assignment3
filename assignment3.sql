8a59ce8f43051363333be3dfe9d80937d776d186
BarborosT <72398699+BarborosT@users.noreply.github.com>
2022-11-27 02:01:17 +0300

    Add files via upload

---
+++ assignment3.sql
@@ -0,0 +1,7 @@
+Select * From country where country like 'A%' and country like '%a'
+
+Select * From country where country like '_______c%'
+
+Select * From film where title like '%T%t%T%t'
+
+Select * From film where title like 'C%' AND length > 90 and rental_rate > 2.99
